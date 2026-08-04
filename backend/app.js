eedeb9a9c88200e76fb1fc13843b199c20b360d0
tanmays329 <tanmays524@gmail.com>
2026-08-04 18:34:06 +0530

    fix: update production API URLs

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
     cors({
         origin: [
-            "http://localhost:3000",
+            "https://task-management-app-jvb6.onrender.com",
             "https://task-management-opu1zc87c-tanmays329s-projects.vercel.app/"
         ],
         credentials: true
